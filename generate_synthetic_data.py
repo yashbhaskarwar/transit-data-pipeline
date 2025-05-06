8b4d27cca844579b4051d0e478cba242f8a50316
Yash <bhaskarwaryash12@gmail.com>
2025-05-05 19:35:02 -0700

    initial config setup for synthetic data

---
+++ generate_synthetic_data.py
@@ -0,0 +1,9 @@
+# DATABASE CONFIGURATION
+
+DB_CONFIG = {
+    'host': 'localhost',
+    'port': 5432,
+    'database': 'transit_delay_optimization',
+    'user': 'postgres',      
+    'password': 'postgres'   
+}
